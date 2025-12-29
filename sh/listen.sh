33b1cc1ea1e9fddce4974134f2b21de80b4d3b84
hyphop <art@khadas.com>
2025-12-29 09:04:58 +0700

    sh/listen.sh:
    Signed-off-by: hyphop <art@khadas.com>

--- sh/listen.sh
+++ sh/listen.sh
@@ -14,33 +14,40 @@ help
 
 GITHUB_USER="Another-Art-Another-Music"
 GITHUB_REPO="listen"
-API_URL="https://api.github.com/repos/$GITHUB_USER/$GITHUB_REPO/releases"
+: ${PAGE:=1}
+: ${PER_PAGE:=100}
+API_URL="https://api.github.com/repos/$GITHUB_USER/$GITHUB_REPO/releases?per_page=$PER_PAGE"
 TMP_JSON="/tmp/github_releases.json"
 PLAYLIST_FILE="/tmp/$GITHUB_USER.m3u"
 
 fetch_and_create_playlist() {
     echo "AAAM: https://another-art-another-music.pages.dev/"
     echo "Fetching releases: $GITHUB_USER/$GITHUB_REPO"
-    curl -s "$API_URL" -o "$TMP_JSON"
+    echo "$API_URL"
+    curl -f -s "$API_URL" -o "$TMP_JSON" || {
+	echo "[e] OOOPS"
+	head -n2 "$TMP_JSON"
+	exit 1
+    }
 
     # Count total releases
     TOTAL_RELEASES=$(jq 'length' "$TMP_JSON")
-    echo "📀 Total Releases Found: $TOTAL_RELEASES"
+    echo "Total Releases Found: $TOTAL_RELEASES"
     TRACKS=$(jq -r '[.[].assets[] | select(.name | endswith(".opus")) | .browser_download_url] | .[]' "$TMP_JSON")
     TOTAL_TRACKS=$(echo "$TRACKS" | wc -l)
 
     [ "$TOTAL_TRACKS" = "0" ] && {
-        echo "❌ No audio tracks found in any release."
+        echo "[W] No audio tracks found in any release."
         exit 1
     }
 
-    echo "🎵 Total Tracks Found: $TOTAL_TRACKS"
+    echo "[v] Total Tracks Found: $TOTAL_TRACKS"
 
     # Save tracks to MPV playlist file
     echo "$TRACKS" > "$PLAYLIST_FILE"
-    echo "✅ Playlist saved to: $PLAYLIST_FILE"
+    echo "[x] Playlist saved to: $PLAYLIST_FILE"
 
-    echo "🎶 Playlist Contents:"
+    echo "@ Playlist Contents:"
     while read l; do
 	echo ${l##*/}
     done < $PLAYLIST_FILE
